8df61105a36a00f56a033574ca739b9d346e22f0
Gerardo Quintana <gerardo.y.quintana@gmail.com>
2025-08-23 23:34:38 -0500

    add task features now has a delete edit and checkmark options and there's a logout button and fucntionality

--- TheVaqueroNetwork/myapp/urls.py
+++ TheVaqueroNetwork/myapp/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/activities/', views.activities, name='activities'),
     path('dashboard/activities/add/', views.add_task, name='add_task'),
+    path('dashboard/activities/edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('dashboard/activities/delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('dashboard/calendar/', views.calendar, name='calendar'),
     path('dashboard/bulletin/', views.bulletin_board, name='bulletin_board'),
     path('dashboard/emergency/', views.emergency_contacts, name='emergency_contacts'),
+    path('logout/', views.log_out, name='log_out'),
 ]
